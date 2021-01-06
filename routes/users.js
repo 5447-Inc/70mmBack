366d11c2a0f18b5a2a47c14ede2d12fea963d5ab
AkhilJ4007 <akhiljamesjoseph944@gmail.com>
2021-01-06 15:32:15 -0500

    Mongoose Complete

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const db = require('../util/database');
-const userController = require('../controllers/userController')
-
-const router = express.Router()
-
-router.post("/addUser",userController.postAddProduct)
-
-module.exports = router 

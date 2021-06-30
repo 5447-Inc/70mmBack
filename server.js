cb157ca8cf3707b03c3e87b39590a78a6ec59ca8
AkhilJ4007 <akhiljamesjoseph944@gmail.com>
2021-06-29 22:21:02 -0400

    cahnged origin policy

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const { connect } = require('mongodb');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', 'https://akj-shopping.herokuapp.com')
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
